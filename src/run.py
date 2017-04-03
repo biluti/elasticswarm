a0db1494ee144f37daeb9d3163087194fbaae36d
Jean-Marc Beguinet <jean-marc.beguinet@parrot.com>
2017-04-03 16:20:41 +0200

    add date and swarm addr in json

--- src/run.py
+++ src/run.py
@@ -6,6 +6,7 @@ import os
 import sys
 import json 
 import time
+import shutil
 import logging
 import argparse
 from datetime import datetime
@@ -35,6 +36,7 @@ def run(swarm_addr, html, elastic_host, refresh_time=1):
 
     REFRESH_IMAGE = 30
     REFRESH_CONTAINER = 5
+    TMP_JSON = '/tmp/dump.json'
     
     
     sc = scheduler.Scheduler()
@@ -54,11 +56,16 @@ def run(swarm_addr, html, elastic_host, refresh_time=1):
         ret = sm.fetch_metrics_greenpool()
         reti = sm.fetch_images_greenpool()
         ret["images_nodes"] = reti
+
+        now = get_update_time()
+        now_formated = now.strftime("%Y-%m-%d @ %H:%M:%S")        
+        ret["update_time"] = now_formated
+        ret["swarm_addr"] = swarm_addr
         
-        with open('/tmp/dump.json', 'w') as fd:
+        with open(TMP_JSON, 'w') as fd:
             fd.write(json.dumps(ret, indent=4))
 
-        with open('/tmp/dump.json', "r") as fd:
+        with open(TMP_JSON, "r") as fd:
             swarm = json.load(fd)
     
         with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'template.html'), "r") as fd:
@@ -67,10 +74,6 @@ def run(swarm_addr, html, elastic_host, refresh_time=1):
         tmpl.globals['human_size'] = utils.human_size
         tmpl.globals['human_uptime'] = utils.human_uptime
         
-        
-
-        now = get_update_time()
-        now_formated = now.strftime("%Y-%m-%d @ %H:%M:%S")
 
         if sc.is_time("image"):
             ei.add_from_swarm(swarm, now)
@@ -85,6 +88,7 @@ def run(swarm_addr, html, elastic_host, refresh_time=1):
         logger.debug("Update time {}".format(now_formated))
 
         
+        shutil.copy(TMP_JSON, os.path.join(html, "swarm.json"))
         pathfile = os.path.join(html, "index.html")
         logger.debug("Write html {}".format(pathfile))
         with open(pathfile, "w") as fd:
